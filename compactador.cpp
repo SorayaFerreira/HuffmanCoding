1605aef541fb8c75870da15dc84084effea2b4b4
iClink <christyanccrd@gmail.com>
2024-05-12 05:01:15 -0300

    Classes Byte e No implementadas para fazer a arvore, implementada guardar as frequencias dos bytes de arquivo em seus respectivos No's

---
+++ compactador.cpp
@@ -0,0 +1,92 @@
+#include <cstdint>
+#include <cstdio>
+
+/* Neste código será feita a implementação de: ler os bytes de um determinado arquivo
+usar um vetor de frequencia( de 0 a 255) para guardar quantas vezes os bytes aparecem, e em seguida guardar
+no vetor índice quais os bytes que tivemos, em seguida fazer nós ultilizando essas informações*/
+
+class Bytes{ // classe para percorrer os Bytes do arquivo
+    private:
+    FILE * file;
+
+    public:
+    Bytes(FILE * file){this->file = file;}
+    int obtem_byte();
+};
+
+
+class No {
+    private:
+
+    int frequencia; // - quantas vezes o byte aparece
+    No * esq; // - filho esquerdo
+    No * dir; // - filho direito
+    uint8_t compacto; // - o código gerado para ser o byte no arquivo compactado
+    bool folha; // - indicador se o nó é galho da arvore ou uma folha
+
+    public:
+
+    No() : frequencia(0), esq(nullptr), dir(nullptr), compacto(0), folha(false) {}
+
+    // Métodos para obter e definir a frequência
+    int getFrequencia() { return this->frequencia;}
+    void setFrequencia(int freq) {this->frequencia += freq;}
+
+    // Métodos para obter e definir o filho esquerdo
+    No * getFilho_esquerdo() {return this->esq;}
+    void setFilho_esquerdo(No * filho) { this->esq = filho;}
+
+    // Métodos para obter e definir o filho direito
+    No * getFilho_direito() {return this->dir;}
+    void setFilho_direito(No * filho) {this->dir = filho;}
+
+    // Métodos para obter e definir o valor compactado
+    uint8_t getCompacto() {return this->compacto;}
+    void setCompacto(uint8_t bin) {this->compacto = bin;}
+
+     // Método para verificar se é folha
+    bool getFolha() {return this->folha;}
+    void setFolha() {this->folha = true;}
+};
+
+int main(){
+
+    No * vetor_frequencia[256]; // vetor de ponteiros para No (objetos)
+
+    // Inicializando o vetor com nullptr
+    for (int i = 0; i < 256; i++) {
+        vetor_frequencia[i] = nullptr;
+    }
+
+    // ponteiro de arquivo 
+    FILE * leitor;
+    leitor = fopen("original.txt", "rb"); //ponteiro de arquivo apontando para arquivo desejado
+
+    int byte; // variável byte;
+
+    Bytes entrada(leitor); // Objeto Bytes chamado "entrada" inicializando com o ponteiro "leitor"
+
+    while (((byte = entrada.obtem_byte()) >= 0) && (byte < 256)){ //percorrendo os bytes do arquivo
+
+        if(vetor_frequencia[byte] == nullptr){ //caso não houver um objeto da classe "No" com determinado byte, criar.
+            vetor_frequencia[byte] = new No(); // criando objeto "No" em novo byte
+            vetor_frequencia[byte]->setFolha(); // ele será uma folha (usado mais para frente)
+            vetor_frequencia[byte]->setFrequencia(1); // sua frequencia passa a ser "1" (0+1)
+        }
+        else
+        {
+            vetor_frequencia[byte]->setFrequencia(1); // Caso contrário, apenas aumenta em 1 sua frequencia
+        }
+    }
+
+
+    return 0;
+}
+
+int Bytes::obtem_byte(){ // Método usado para extrair os Bytes do arquivo
+
+    uint8_t byte;
+
+    if(fread(&byte,1,1,file) == 0){return -1;} // caso não houver mais bytes, retorna "-1", quebrando o "while" da main.
+    return byte;
+}
